d8d6cac4f4b8961a78107ab5d50879b1511c04bd
Shubham Jangle <jangleshubham13@gmail.com>
2023-06-25 23:58:51 +0530

    Added Main Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,30 @@
+import Navbar from "@/components/navbar";
+
 export default function Home() {
+  const output = `Laborum adipisicing sunt dolor ullamco duis. Nisi nostrud nulla ea ad dolor incididunt ex minim laboris ad. Consequat amet nostrud mollit magna adipisicing occaecat do dolore quis anim. Id eu amet voluptate nisi sint duis consequat aute Lorem
+Aliquip do nulla occaecat aliquip est proident dolor tempor ex. Culpa fugiat cupidatat nulla laboris magna quis sit. Eu adipisicing sint tempor cillum ut quis sunt enim eu pariatur magna. Nulla exercitation minim Lorem qui est voluptate. Nostrud amet cupidatat ut exercitation ea dolor est eiusmod laboris. Fugiat veniam proident in cupidatat Lorem ipsum est dolore officia sint incididunt consequat labore.
+
+Laborum commodo deserunt aliqua cupidatat cillum dolor velit veniam esse deserunt ut magna non dolor. Culpa dolore tempor ex reprehenderit eiusmod voluptate quis dolor dolore qui aliqua voluptate. Adipisicing ullamco Lorem excepteur laboris sunt magna est et eu voluptate. Labore irure irure culpa aliquip aliqua velit ad laborum culpa aliquip aute aute non. Duis et esse commodo minim ea aliqua nostrud anim. Nostrud occaecat non consequat elit.
+Irure excepteur dolore aute veniam commodo id laborum sit exercitation laborum reprehenderit consectetur quis qui. Aliquip incididunt laboris veniam commodo irure pariatur. Esse qui ad minim quis fugiat cupidatat quis cillum."
+`;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button className="btn btn-primary">Whatsupp Cerence</button>
-    </main>
+    <div>
+      <Navbar />
+      <main className="px-8 md:px-20 mt-8">
+        <div className="">
+          <div className="min-h-16">
+            <textarea
+              placeholder="Type your query here..."
+              className="textarea textarea-bordered textarea-primary w-full"
+            ></textarea>
+            <button className="btn btn-primary w-full">Send</button>
+          </div>
+          <div className="mt-8"></div>
+          <div className="card shadow-md card-bordered border-primary">
+            <div className="card-body">{output}</div>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,197 @@
+"use client";
+
+import Image from "next/image";
+import React, { useRef, useState, useEffect } from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+
+  // Using ref and event listener to close the popup menu items when clicked elsewhere on screen
+  const menuRef = useRef(null);
+  const profileRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target) &&
+      profileRef.current &&
+      !profileRef.current.contains(event.target)
+    ) {
+      setIsMenuOpen(false);
+      setIsProfileOpen(false);
+    }
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const toggleProfile = () => {
+    setIsProfileOpen((isProfileOpen) => !isProfileOpen);
+  };
+
+  const getLinks = () => {
+    return (
+      <>
+        {/* Public Links */}
+        {/* <Link
+          href="/"
+          className="px-3 py-2 rounded-md text-sm font-medium hover:bg-base-300"
+        >
+          Home
+        </Link>
+        <Link
+          href="/public"
+          className="px-3 py-2 rounded-md text-sm font-medium hover:bg-base-300"
+        >
+          Public
+        </Link> */}
+      </>
+    );
+  };
+
+  return (
+    <nav className="bg-base-100 shadow-md">
+      {/* Desktop View */}
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          {/* Logo and App Name */}
+          <div className="flex-shrink-0 flex items-center cursor-pointer">
+            <div className="w-8 h-8 relative">
+              <Image className="rounded-full" src="/logo.png" alt="Logo" fill />
+            </div>
+            <span className="text-lg font-semibold mx-2">ChatAI</span>
+          </div>
+
+          {/* Menu Items */}
+          <div ref={menuRef} className="hidden md:block">
+            <div className="flex">
+              <div className="ml-10 flex items-baseline space-x-4">
+                {getLinks()}
+              </div>
+
+              {/* Hamburger icon and User Profile */}
+              <div ref={profileRef} className="ml-4 flex items-center">
+                <div className="relative">
+                  <div>
+                    <button
+                      className="max-w-xs rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2"
+                      id="user-menu"
+                      aria-haspopup="true"
+                      onClick={toggleProfile}
+                    >
+                      <span className="sr-only">User Menu</span>
+                      <svg
+                        className="block h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+
+                  {/* Dropdown */}
+                  {isProfileOpen && (
+                    <div
+                      className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg ring-1 ring-opacity-5 focus:outline-none bg-base-100"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="user-menu"
+                    >
+                      <div className="py-1" role="none">
+                        <a
+                          className="block px-4 py-2 text-sm hover:bg-base-300 cursor-pointer"
+                          role="menuitem"
+                        >
+                          About
+                        </a>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Mobile Menu */}
+          <div ref={menuRef} className="-mr-2 flex md:hidden">
+            <button
+              className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2"
+              onClick={toggleMenu}
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isMenuOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu Dropdown */}
+      {isMenuOpen && (
+        <div className="md:hidden">
+          <div className="flex flex-wrap px-2 py-2 gap-2">{getLinks()}</div>
+          <div className="px-2 py-3 border-t border-gray-700">
+            <div className="mt-3 px-2 space-y-1">
+              <a
+                href="#"
+                className="block px-3 py-2 rounded-md text-base font-medium hover:bg-base-300"
+              >
+                About
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
