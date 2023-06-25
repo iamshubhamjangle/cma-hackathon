2d10d7293b873db3b80472fb4c9b4b29559a375e
Shubham Jangle <jangleshubham13@gmail.com>
2023-06-25 20:49:44 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  theme: {},
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#00B4BE",
+          secondary: "#3a82ee",
+          accent: "#f4a564",
+          neutral: "#2b3440",
+          "base-100": "#ffffff",
+          info: "#a8c7eb",
+          success: "#67daac",
+          warning: "#fbb36a",
+          error: "#e4695e",
+        },
       },
-    },
+    ],
   },
-  plugins: [],
-}
+};
