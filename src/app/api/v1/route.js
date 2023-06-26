9c43868062a149c112c550f756b330563392c450
Shubham Jangle <jangleshubham13@gmail.com>
2023-06-26 15:07:21 +0530

    Added API route

--- src/app/api/v1/route.js
+++ src/app/api/v1/route.js
@@ -1,13 +1,61 @@
 import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
 
 export async function GET() {
-  //   const res = await fetch('https://data.mongodb-api.com/...', {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'API-Key': process.env.DATA_API_KEY,
-  //     },
-  //   })
-  //   const data = await res.json()
-
   return NextResponse.json({ status: "API is up and running!" });
+}
+
+export async function POST(req) {
+  try {
+    const reqBody = await req.json();
+    const messages = [];
+
+    // Check and push User content to messages
+    if (reqBody.userContent === undefined) {
+      return NextResponse.json({
+        status: "failed",
+        error: "userContent is required property!",
+      });
+    }
+
+    messages.push({ role: "user", content: reqBody.userContent });
+
+    // Check and push System content to messages
+    if (reqBody.systemContent !== undefined) {
+      messages.push({ role: "system", content: reqBody.systemContent });
+    }
+
+    // Check and push Assistant content to messages
+    if (reqBody.assistantContent !== undefined) {
+      messages.push({ role: "assistant", content: reqBody.assistantContent });
+    }
+
+    // Create new instance of openAi
+    const openAi = new OpenAIApi(
+      new Configuration({
+        apiKey: process.env.CHAT_GPT_API_KEY,
+      })
+    );
+
+    // Await the response from OpenAI
+    const response = await openAi.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages,
+    });
+
+    // Send back the response
+    return NextResponse.json({
+      status: "success",
+      result: response.data.choices[0].message.content,
+    });
+  } catch (e) {
+    // Handle Errors
+    console.log("----------START---------");
+    console.log(e);
+    console.log("-----------END----------");
+    return NextResponse.json({
+      status: "failed",
+      error: "We are unable to process this request. :(",
+    });
+  }
 }
