5343d2b0b16d12803f8e8fdf7f3888482de26d7f
Shubham Jangle <jangleshubham13@gmail.com>
2023-06-26 15:07:33 +0530

    Added Content Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,13 @@
+import Content from "@/components/content";
 import Navbar from "@/components/navbar";
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
-  const output = `Laborum adipisicing sunt dolor ullamco duis. Nisi nostrud nulla ea ad dolor incididunt ex minim laboris ad. Consequat amet nostrud mollit magna adipisicing occaecat do dolore quis anim. Id eu amet voluptate nisi sint duis consequat aute Lorem
-Aliquip do nulla occaecat aliquip est proident dolor tempor ex. Culpa fugiat cupidatat nulla laboris magna quis sit. Eu adipisicing sint tempor cillum ut quis sunt enim eu pariatur magna. Nulla exercitation minim Lorem qui est voluptate. Nostrud amet cupidatat ut exercitation ea dolor est eiusmod laboris. Fugiat veniam proident in cupidatat Lorem ipsum est dolore officia sint incididunt consequat labore.
-
-Laborum commodo deserunt aliqua cupidatat cillum dolor velit veniam esse deserunt ut magna non dolor. Culpa dolore tempor ex reprehenderit eiusmod voluptate quis dolor dolore qui aliqua voluptate. Adipisicing ullamco Lorem excepteur laboris sunt magna est et eu voluptate. Labore irure irure culpa aliquip aliqua velit ad laborum culpa aliquip aute aute non. Duis et esse commodo minim ea aliqua nostrud anim. Nostrud occaecat non consequat elit.
-Irure excepteur dolore aute veniam commodo id laborum sit exercitation laborum reprehenderit consectetur quis qui. Aliquip incididunt laboris veniam commodo irure pariatur. Esse qui ad minim quis fugiat cupidatat quis cillum."
-`;
   return (
     <div>
+      <Toaster />
       <Navbar />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-8">
-        <div className="">
-          <div className="min-h-16">
-            <textarea
-              placeholder="Type your query here..."
-              className="textarea textarea-bordered textarea-primary w-full"
-            ></textarea>
-            <button className="btn btn-primary w-full">Send</button>
-          </div>
-          <div className="mt-8"></div>
-          <div className="card shadow-md card-bordered border-primary">
-            <div className="card-body">{output}</div>
-          </div>
-        </div>
-      </main>
+      <Content />
     </div>
   );
 }
---
+++ src/components/content.js
@@ -0,0 +1,86 @@
+"use client";
+
+import axios from "axios";
+import Image from "next/image";
+import { useState } from "react";
+
+const Content = () => {
+  const [result, setResult] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    await axios
+      .post("/api/v1", {
+        userContent: userInput,
+      })
+      .then((res) => setResult(res.data.result))
+      .catch((err) => {
+        toast.error("Something went wrong!");
+        console.log(err);
+      })
+      .finally(() => setLoading(false));
+  };
+
+  return (
+    <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-8">
+      <div className="min-h-16">
+        <textarea
+          placeholder="Type your query here..."
+          className="textarea textarea-bordered textarea-primary w-full"
+          onChange={(e) => setUserInput(e.target.value)}
+        ></textarea>
+        <button
+          className="btn btn-primary w-full"
+          onClick={handleSubmit}
+          disabled={loading}
+        >
+          {loading ? (
+            <span className="loading loading-dots loading-md"></span>
+          ) : (
+            "Send"
+          )}
+        </button>
+      </div>
+      <div className="mt-8"></div>
+      <div className="card shadow-md justify-center card-bordered border-primary">
+        {!result && !loading && (
+          <div className="flex flex-col items-center gap-8 my-16">
+            <Image
+              src="empty.svg"
+              height={360}
+              width={360}
+              alt="Try searching something!"
+            />
+            <p className="font-bold text-xl">What&apos;s in you mind?</p>
+          </div>
+        )}
+        {loading && (
+          <div role="status" className="card-body space-y-2.5 animate-pulse">
+            <div className="flex items-center w-full space-x-2">
+              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-32"></div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-24"></div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
+            </div>
+            <div className="flex items-center w-full space-x-2 max-w-[720px]">
+              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-full"></div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-24"></div>
+            </div>
+            <div className="flex items-center w-full space-x-2 max-w-[480px]">
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
+              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-80"></div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
+            </div>
+            <span className="sr-only">Loading...</span>
+          </div>
+        )}
+
+        {result && !loading && <div className="card-body">{result}</div>}
+      </div>
+    </main>
+  );
+};
+
+export default Content;
