7e3a72cab05f4fe24673511af15830c665842516
Shubham Jangle <jangleshubham13@gmail.com>
2023-06-28 18:49:53 +0530

    Updated footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,31 +3,33 @@ import { AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer
-      className="flex justify-between p-4"
+    <div
+      className=""
       style={{
         boxShadow: "0px -4px 3px rgba(50, 50, 50, 0.10)",
       }}
     >
-      <div className="items-center grid-flow-col">
-        <Image
-          className="rounded-full"
-          width={28}
-          height={28}
-          src="/logo.png"
-          alt="Logo"
-        />
-      </div>
-      <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <a
-          target="_blank"
-          href="https://github.com/iamshubhamjangle/cma-hackathon"
-          rel="noopener noreferrer"
-        >
-          <AiFillGithub size={28} />
-        </a>
-      </div>
-    </footer>
+      <footer className="flex justify-between max-w-7xl mx-auto p-4 sm:px-6 lg:px-8">
+        <div className="items-center grid-flow-col">
+          <Image
+            className="rounded-full"
+            width={28}
+            height={28}
+            src="/logo.png"
+            alt="Logo"
+          />
+        </div>
+        <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+          <a
+            target="_blank"
+            href="https://github.com/iamshubhamjangle/cma-hackathon"
+            rel="noopener noreferrer"
+          >
+            <AiFillGithub size={28} />
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 };
 
